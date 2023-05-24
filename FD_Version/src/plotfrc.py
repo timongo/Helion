fbff26788b7568c9800e0d57df2f9acda3388861
rpgiridharan <rpgiridharan@gmail.com>
2023-05-24 09:54:23 +0200

    updated plotfrc

---
+++ FD_Version/src/plotfrc.py
@@ -0,0 +1,21 @@
+import readxtor as rx
+import plotxtor as px
+import numpy as np
+import matplotlib.pyplot as plt
+
+Z = rx.RawReadField('FRC','ZZ')
+R = rx.RawReadField('FRC','RR')
+psi = rx.RawReadField('FRC','psi')
+plt.figure(1)
+plt.clf()
+px.Figure(Z,R,psi,equal=False)
+plt.xlabel('Z'); plt.ylabel('R')
+
+P = rx.RawReadField('FRC','Pressure')
+s = rx.RawReadField('FRC','s')
+J = rx.RawReadField('FRC','JacobMesh')
+Vprime = np.mean(J[:,:-1],axis=1)
+plt.figure(2)
+plt.clf()
+px.Plot(s,P*Vprime**(5/3),lw=2)
+plt.xlabel('$s$'); plt.ylabel('$PV\'^{5/3}$')
